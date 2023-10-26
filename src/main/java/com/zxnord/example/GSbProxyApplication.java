94b54168c2c8a1a05a8d2c0f8218788d004c69cd
Edson Contreras <edrecon@gmail.com>
2023-10-26 00:46:12 -0300

    Everything begins with a First Commitgit add .

---
+++ src/main/java/com/zxnord/example/GSbProxyApplication.java
@@ -0,0 +1,13 @@
+package com.zxnord.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GSbProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GSbProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zxnord/example/GSbProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxnord.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GSbProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
